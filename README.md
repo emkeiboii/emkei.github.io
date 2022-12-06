a49a4084d7ddc792af5a87a2f47d246c3547b098
emkeiboii <114905665+emkeiboii@users.noreply.github.com>
2022-12-06 12:04:34 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal_Website
+
+Personal website to be a portfolio
+
+Started this project on the 25th of october.
---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+  var element = document.getElementById("mobile-nav");
+  element.classList.toggle("open");
+}
